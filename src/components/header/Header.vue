f88f694a8cb1da549d9c7bdd0b32604eca5c3213
wangqi <winwangqi@live.cn>
2017-04-25 00:56:08 +0800

    header基本完成，正在完成support详情

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -12,24 +12,248 @@
         <div class="description">
           {{ seller.description }}/{{ seller.deliveryTime }}分钟送达
         </div>
+        <div v-if="seller.supports" class="support">
+          <span class="icon" :class="classMap[seller.supports[0].type]"></span>
+          <span class="text">{{ seller.supports[0].description }}</span>
+        </div>
+      </div>
+      <div v-if="seller.supports" class="support-count" @click="showDetail">
+        <span class="count">{{ seller.supports.length }}个</span>
+        <i class="icon-keyboard_arrow_right"></i>
       </div>
     </div>
-    <div class="bulletin-wrapper">
-
+    <div class="bulletin-wrapper" @click="showDetail">
+      <span class="bulletin-title"></span><span class="bulletin-text">
+        粥品香坊其烹饪粥料的秘方源于中国千年古法，在融和现代制作工艺，由世界烹饪大师屈浩先生领衔研发。坚守纯天然、0添加的良心品质深得消费者青睐，发展至今成为粥类的引领品牌。是2008年奥运会和2013年园博会指定餐饮服务商。
+      </span>
+      <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background">
+      <img :src="seller.avatar" alt="background" width="100%" height="100%">
+    </div>
+    <div v-show="detailShow" class="detail">
+      <div class="detail-wrapper clearfix">
+        <div class="detail-main"></div>
+      </div>
+      <div class="detail-close">
+        <i class="icon-close"></i>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  props: {
-    seller: {
-      type: Object
+  export default {
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    data() {
+      return {
+        detailShow: false
+      };
+    },
+    methods: {
+      showDetail() {
+        this.detailShow = true;
+      }
+    },
+    created() {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
     }
-  }
-};
+  };
 </script>
 
 <style lang="less">
+  @import "../../common/less/mixin.less";
 
+  .header {
+    position: relative;
+    overflow: hidden;
+    color: #fff;
+    background: rgba(7, 17, 27, .5);
+
+    .content-wrapper {
+      position: relative;
+      padding: 24px 12px 18px 24px;
+      font-size: 0;
+
+      .avatar {
+        display: inline-block;
+        vertical-align: top;
+
+        img {
+          border-radius: 2px;
+        }
+      }
+
+      .content {
+        display: inline-block;
+        margin-left: 16px;
+
+        .title {
+          margin: 2px 0 8px 0;
+
+          .brand {
+            display: inline-block;
+            vertical-align: top;
+            width: 30px;
+            height: 18px;
+
+            .bg-image("../../components/header/brand");
+            background-size: 30px 18px;
+            background-repeat: no-repeat;
+          }
+
+          .name {
+            margin-left: 6px;
+            font-size: 16px;
+            line-height: 18px;
+            font-weight: bold;
+          }
+        }
+
+        .description {
+          margin-bottom: 10px;
+          line-height: 12px;
+          font-size: 12px;
+        }
+
+        .support {
+          .icon {
+            display: inline-block;
+            vertical-align: top;
+            width: 12px;
+            height: 12px;
+            margin-right: 4px;
+            background-size: 12px 12px;
+            background-repeat: no-repeat;
+
+            &.decrease {
+              .bg-image("../../components/header/decrease_1");
+            }
+
+            &.discount {
+              .bg-image("../../components/header/discount_1");
+            }
+
+            &.guarantee {
+              .bg-image("../../components/header/guarantee_1");
+            }
+
+            &.invoice {
+              .bg-image("../../components/header/invoice_1");
+            }
+
+            &.special {
+              .bg-image("../../components/header/special_1");
+            }
+          }
+
+          .text {
+            line-height: 12px;
+            font-size: 10px;
+            color: #fff;
+
+          }
+        }
+      }
+
+      .support-count {
+        position: absolute;
+        right: 12px;
+        bottom: 14px;
+        padding: 0 8px;
+        height: 24px;
+        line-height: 24px;
+        border-radius: 14px;
+        background: rgba(0, 0, 0, .2);
+        text-align: center;
+
+        .count {
+          vertical-align: top;
+          font-size: 10px;
+        }
+
+        .icon-keyboard_arrow_right {
+          margin-left: 2px;
+          line-height: 24px;
+          font-size: 10px;
+        }
+      }
+    }
+
+    .bulletin-wrapper {
+      position: relative;
+      height: 28px;
+      line-height: 28px;
+      padding: 0 22px 0 12px;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      background: rgba(7, 17, 27, .2);
+
+      .bulletin-title {
+        display: inline-block;
+        vertical-align: top;
+        margin-top: 8px;
+        width: 22px;
+        height: 12px;
+        .bg-image("../../components/header/bulletin");
+        background-size: 22px 12px;
+        background-repeat: no-repeat;
+      }
+
+      .bulletin-text {
+        font-size: 10px;
+      }
+
+      .icon-keyboard_arrow_right {
+        position: absolute;
+        top: 8px;
+        right: 12px;
+        font-size: 10px;
+      }
+    }
+
+    .background {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      z-index: -1;
+      filter: blur(10px);
+    }
+
+    .detail {
+      position: fixed;
+      top: 0;
+      left: 0;
+      z-index: 100;
+      width: 100%;
+      height: 100%;
+      overflow: auto;
+      background: rgba(7, 17, 27, .8);
+
+      .detail-wrapper {
+        min-height: 100%;
+
+        .detail-main {
+          margin-top: 64px;
+          padding-bottom: 64px;
+        }
+      }
+
+      .detail-close {
+        position: relative;
+        width: 32px;
+        height: 32px;
+        margin: -64px auto 0 auto;
+        clear: both;
+        font-size: 32px;
+      }
+    }
+  }
 </style>
